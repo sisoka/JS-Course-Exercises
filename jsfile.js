1656e4d27b11800c2c6a04a65ad50e8c5fdcab1f
sisko <sisoka1988@abv.bg>
2024-02-28 20:35:24 +0200

    js function added for changing the color of the h1. Btn added in index.html

--- jsfile.js
+++ jsfile.js
@@ -1,6 +1,17 @@
-function changeColor (element, updatedColor){
-    element.style.color = updatedColor;
-}
+// function changeColor (element, updatedColor){
+//     element.style.color = updatedColor;
+// }
 
-var title = document.getElementsByName("title");
-changeColor(title, "red");
+var title = document.getElementById("h1");
+// var button = document.getElementById("myButton");
+
+
+// button.addEventListener("click", changeColor.bind(title,'red'));
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Code to manipulate elements goes here
+    var button = document.querySelector("button");
+    button.addEventListener("click", function() {
+      title.style.color = 'red';
+    });
+  });
